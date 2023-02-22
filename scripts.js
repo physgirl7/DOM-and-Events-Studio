c1673df8a86e8a3e32160e3bc3c758fedbd457f2
Julie Pitney <julie@pitney.org>
2023-02-21 19:41:02 -0600

    first save of scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,38 @@
+function init(){
+    const takeOffButton = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const landingButton = document.getElementById("landing");
+    const abortButton = document.getElementById("missionAbort");
+
+    takeOffButton.addEventListener("click", function(event){
+    const confirmTakeoff = window.confirm("Confirm that the shuttle is ready for takeoff");
+    if (confirmTakeoff){
+        flightStatus.innerHTML = "Shuttle in flight";
+        shuttleBackground.style.backgroundColor = "blue";
+        spaceShuttleHeight.innerHTML = 10000;
+    }
+})
+
+    landingButton.addEventListener("click", function(event){
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = 0;
+    })
+
+    abortButton.addEventListener("click", function(event){
+        const confirmAbort = window.confirm("Confirm that you want to abort the mission.");
+        if (confirmAbort){
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = 0;
+        }
+    })
+
+}
+
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", init);
