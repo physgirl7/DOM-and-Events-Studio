bee45c2587f353a101559f0738dd74ea4a629239
Julie Pitney <julie@pitney.org>
2023-02-21 22:01:45 -0600

    second commit for scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,16 @@ function init(){
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
     const landingButton = document.getElementById("landing");
     const abortButton = document.getElementById("missionAbort");
+    const rocketImage = document.getElementById("rocket");
+    const upButton = document.getElementById("up");
+    const downButton = document.getElementById("down");
+    const rightButton = document.getElementById("right");
+    const leftButton = document.getElementById("left");
+    const flightDisplay = document.getElementById("flightDisplay");
+
+    rocketImage.style.position = "absolute";
+    rocketImage.style.bottom = "0px";
+    rocketImage.style.right = "200px";
 
     takeOffButton.addEventListener("click", function(event){
     const confirmTakeoff = window.confirm("Confirm that the shuttle is ready for takeoff");
@@ -20,6 +30,8 @@ function init(){
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = "green";
         spaceShuttleHeight.innerHTML = 0;
+        rocketImage.style.bottom = "0px";
+        rocketImage.style.right = "200px";
     })
 
     abortButton.addEventListener("click", function(event){
@@ -28,9 +40,51 @@ function init(){
             flightStatus.innerHTML = "Mission aborted.";
             shuttleBackground.style.backgroundColor = "green";
             spaceShuttleHeight.innerHTML = 0;
+            rocketImage.style.bottom = "0px";
+            rocketImage.style.right = "200px";
         }
     })
 
+    upButton.addEventListener("click", function(event){
+        // console.log(parseInt(rocketImage.style.bottom));
+        let originalPositionUp = parseInt(rocketImage.style.bottom);
+        let newPositionUp = originalPositionUp + 10;
+        let newPositionUpPixels = newPositionUp +"px";
+        rocketImage.style.bottom = newPositionUpPixels;
+        let originalHeight = parseInt(spaceShuttleHeight.innerHTML);
+        let newHeight = originalHeight + 10000;
+        spaceShuttleHeight.innerHTML = newHeight;
+    })
+
+    downButton.addEventListener("click", function(event){
+        let originalPosition = parseInt(rocketImage.style.bottom);
+        let newPosition = originalPosition - 10;
+        let newPositionPixels = newPosition + "px";
+        rocketImage.style.bottom = newPositionPixels;
+        let originalHeight = parseInt(spaceShuttleHeight.innerHTML);
+        let newHeight = originalHeight -10000;
+        spaceShuttleHeight.innerHTML = newHeight;
+    })
+
+    rightButton.addEventListener("click", function(event){
+        let originalPosition = parseInt(rocketImage.style.right);
+        let newPosition = originalPosition - 10;
+        let newPositionPixels = newPosition + "px";
+        rocketImage.style.right = newPositionPixels;
+    })
+
+    leftButton.addEventListener("click", function(event){
+        let originalPosition = parseInt(rocketImage.style.right);
+        let newPosition = originalPosition + 10;
+        let newPositionPixels = newPosition + "px";
+        rocketImage.style.right = newPositionPixels;
+    })
+
+    // function pastMarginLimitBottom (rocketImage.style.bottom){
+    //     let marginNumber = parseInt(rocketImage.style.bottom);
+    //     if (marginNumber> )
+        
+    // }
 }
 
 // Write your JavaScript code here.
